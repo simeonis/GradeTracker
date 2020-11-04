a48a53ee660c20207e8dea516625567e42fa370a
Shae <56325172+simeonis@users.noreply.github.com>
2020-11-03 20:47:36 -0500

    Added necessary dependencies (data-binding & viewModel) Added needed empty fragments

---
+++ app/src/main/java/sheridan/simeoni/gradetracker/ui/CourseFragment.kt
@@ -0,0 +1,29 @@
+package sheridan.simeoni.gradetracker.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import sheridan.simeoni.gradetracker.R
+
+class CourseFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_course, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_course).setOnClickListener {
+            findNavController().navigate(R.id.action_course_to_assignment)
+        }
+    }
+}
--- app/src/main/java/sheridan/simeoni/gradetracker/ui/StartFragment.kt
+++ app/src/main/java/sheridan/simeoni/gradetracker/ui/StartFragment.kt
@@ -1,4 +1,4 @@
-package sheridan.simeoni.gradetracker
+package sheridan.simeoni.gradetracker.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,25 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import sheridan.simeoni.gradetracker.R
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class StartFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_start, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        view.findViewById<Button>(R.id.button_start).setOnClickListener {
+            findNavController().navigate(R.id.action_start_to_year)
         }
     }
 }
