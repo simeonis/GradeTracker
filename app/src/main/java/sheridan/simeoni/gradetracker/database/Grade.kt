a0c7deacd5cec59b6f1695350ac6d4a58683c3ae
Seth Climenhaga <climense@sheridanCollege.ca>
2020-11-08 19:11:56 -0500

    Setup Basic DataBase and Structure

---
+++ app/src/main/java/sheridan/simeoni/gradetracker/database/Grade.kt
@@ -0,0 +1,26 @@
+package sheridan.simeoni.gradetracker.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "Grade")
+data class Grade(
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Long,
+
+    @ColumnInfo(name = "Assignment_Name")
+    val assignmentTitle: String,
+
+    @ColumnInfo(name = "Grade")
+    val grade: Int,
+
+    @ColumnInfo(name = "Weight")
+    val weight: Int,
+
+    @ColumnInfo(name = "Target_Grade")
+    val targetGrade: Int,
+
+    )
