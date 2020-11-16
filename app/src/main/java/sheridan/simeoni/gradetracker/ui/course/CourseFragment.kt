a39c0e5535e925bc67766540a2c7beef71292d98
Shae <56325172+simeonis@users.noreply.github.com>
2020-11-16 01:48:42 -0500

    Fixed backStack duplication bug

--- app/src/main/java/sheridan/simeoni/gradetracker/ui/course/CourseFragment.kt
+++ app/src/main/java/sheridan/simeoni/gradetracker/ui/course/CourseFragment.kt
@@ -1,10 +1,10 @@
 package sheridan.simeoni.gradetracker.ui.course
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.fragment.findNavController
@@ -45,10 +45,10 @@ class CourseFragment : Fragment() {
         binding.courseAddButton.setOnClickListener { findNavController().navigate(R.id.action_course_to_courseDialog) }
 
         val savedStateHandle: SavedStateHandle? = findNavController().currentBackStackEntry?.savedStateHandle
-        savedStateHandle?.getLiveData<CourseDialogData>(CourseDialog.CONFIRMATION_COURSE_RESULT)?.observe(viewLifecycleOwner)
+        savedStateHandle?.set(CourseDialog.CONFIRMATION_RESULT, null) // Dialog will override this
+        savedStateHandle?.getLiveData<CourseDialogData>(CourseDialog.CONFIRMATION_RESULT)?.observe(viewLifecycleOwner)
         {
-            viewModel.add(it.name, it.targetGrade)
-            savedStateHandle.remove<CourseDialogData>(CourseDialog.CONFIRMATION_COURSE_RESULT)
+            if (it != null) viewModel.add(it.name, it.targetGrade)
         }
         savedStateHandle?.getLiveData<Long>(ConfirmationDialog.CONFIRMATION_RESULT)?.observe(viewLifecycleOwner)
         {
