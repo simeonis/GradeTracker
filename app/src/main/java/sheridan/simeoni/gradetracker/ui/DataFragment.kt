1bff75e246653f67eeebd8616c8f4362976d210a
Seth Climenhaga <climense@sheridanCollege.ca>
2020-11-20 15:36:41 -0500

    Added dynamic interaction to Data Fragment

--- app/src/main/java/sheridan/simeoni/gradetracker/ui/DataFragment.kt
+++ app/src/main/java/sheridan/simeoni/gradetracker/ui/DataFragment.kt
@@ -1,5 +1,6 @@
 package sheridan.simeoni.gradetracker.ui
 
+
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -7,8 +8,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
-import sheridan.simeoni.gradetracker.R
 import sheridan.simeoni.gradetracker.databinding.FragmentDataBinding
 
 class DataFragment : Fragment() {
@@ -18,23 +19,39 @@ class DataFragment : Fragment() {
 
         binding = FragmentDataBinding.inflate(inflater, container, false)
         binding.dataNextButton.setOnClickListener { next() }
-
+        binding.fragmentDataFillerGrade.isVisible = false
 
         return binding.root
     }
-
+    var clickCount = 0
     private fun next(){
         val sharedPreferences : SharedPreferences = requireActivity().getSharedPreferences("My_Prefs", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
 
         val name = binding.fragmentDataNameInput.text.toString()
-        val fillerGrade = binding.fragmentDataFillerGrade.text.toString().toInt()
-        editor.putString("Name", name)
-        editor.putInt("filler_grade", fillerGrade)
-        editor.apply()
+        val fillerGrade = binding.fragmentDataFillerGrade.text.toString()
 
-        findNavController().navigate(DataFragmentDirections.actionDataToTerm())
+        clickCount +=1;
+        if(!name.isEmpty()){
+            binding.fragmentDataNameInput.isVisible = false
+            binding.fragmentDataFillerGrade.isVisible = true
+            editor.putString("Name", name)
+
+        }
+        else{
+            binding.fragmentDataNameInput.error = "required"
+        }
+
+        if(!fillerGrade.isEmpty()){
+            editor.putInt("filler_grade", fillerGrade.toInt())
+        }
+        else if(fillerGrade.isEmpty() && clickCount > 1){
+            binding.fragmentDataFillerGrade.error = "required"
+        }
+
+        if(!name.isEmpty() && !fillerGrade.isEmpty()){
+            editor.apply()
+            findNavController().navigate(DataFragmentDirections.actionDataToTerm())
+        }
     }
-
-
 }
