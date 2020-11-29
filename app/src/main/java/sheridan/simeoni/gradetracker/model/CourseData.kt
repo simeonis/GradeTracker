e6908ccd080026341122271de9f312b24e6aa94a
Shae <56325172+simeonis@users.noreply.github.com>
2020-11-28 20:02:10 -0500

    Added SafeArgs to GradeFragment

--- app/src/main/java/sheridan/simeoni/gradetracker/model/CourseData.kt
+++
@@ -1,5 +0,0 @@
-package sheridan.simeoni.gradetracker.model
-
-import java.io.Serializable
-
-data class CourseData(var name: String, var assignmentData: List<AssignmentData>): Serializable
--- app/src/main/java/sheridan/simeoni/gradetracker/ui/grade/GradeFragment.kt
+++ app/src/main/java/sheridan/simeoni/gradetracker/ui/grade/GradeFragment.kt
@@ -5,17 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
-import sheridan.simeoni.gradetracker.R
+import android.widget.SeekBar
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import sheridan.simeoni.gradetracker.databinding.FragmentGradeBinding
+import sheridan.simeoni.gradetracker.ui.course.CourseFragmentArgs
 
 class GradeFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private lateinit var binding: FragmentGradeBinding
+    private val safeArgs: GradeFragmentArgs by navArgs()
+    private val viewModel: GradeViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grade, container, false)
+        binding = FragmentGradeBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        activity?.title = safeArgs.keyEnvelope.title
+
+        binding.gradeUpdateButton.setOnClickListener { updateGrade() }
+        binding.gradeSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                binding.gradeTotalNumberLabel.text = progress.toFloat().toString()
+                binding.gradeTotalProgress.progress = progress
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+        return binding.root
+    }
+
+    private fun updateGrade() {
+        val gradeEarned = binding.gradeEarnedInput
+        if (gradeEarned.text.isEmpty()) {
+            gradeEarned.error = "required"
+        } else {
+            viewModel.updateGrade(gradeEarned.text.toString().toInt())
+        }
     }
 }
